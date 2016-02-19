f6873011c519380ae5dfa23d3606cadf3982c21e
Zhi Zhou <zhi.zhou@amlogic.com>
2016-02-19 07:46:02 -0500

    PD#117692 fixed libplayer some lib depends bug
    Change-Id: I31044425f47e53c293bf5bcc813b78b9e746d5ad

--- examples/Android.mk
+++ examples/Android.mk
@@ -12,7 +12,7 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../amplayer/player/include \
     $(JNI_H_INCLUDE) \
     $(LOCAL_PATH)/../streamsource \
 
-LOCAL_STATIC_LIBRARIES := libamplayer libamplayer libamcodec libavformat librtmp libavcodec libavutil libamadec libamavutils libamstreaming libiconv
+LOCAL_STATIC_LIBRARIES := libamplayer libamplayer libamcodec libavformat librtmp libavcodec libavutil libamadec libamavutils  libiconv
 LOCAL_SHARED_LIBRARIES += libutils libmedia libbinder libz libdl libcutils libssl libcrypto libamsubdec
 
 LOCAL_SHARED_LIBRARIES += libbinder 
--- examples/kplayer.c
+++ examples/kplayer.c
@@ -205,7 +205,6 @@ int main(int argc, char *argv[])
         return -1;
     }
     player_init();
-    streamsource_init();
     set_display_axis(0);        //move osd out of screen to set video layer out
 
     player_register_update_callback(&pCtrl->callback_fn, &update_player_info, 1000);
