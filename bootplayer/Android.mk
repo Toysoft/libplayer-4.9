7cab82be96355912e1dbf306e44a170d6fcb1faf
Zhi Zhou <zhi.zhou@amlogic.com>
2016-03-10 06:06:37 -0500

    PD#117692 fixed compiler error on some env
    Change-Id: I76b095fca35e79d2bbf228273777bdf8fe6234bd

--- bootplayer/Android.mk
+++ bootplayer/Android.mk
@@ -19,7 +19,6 @@ ifneq (0, $(shell expr $(PLATFORM_VERSION) \>= 5.0))
 LOCAL_STATIC_LIBRARIES := libamplayer libamplayer libamcodec libavformat librtmp libavcodec libavutil libamadec_alsa  libiconv
 LOCAL_SHARED_LIBRARIES += libutils libmedia libbinder libz libdl libcutils libssl libcrypto libasound libamavutils libamsubdec
 else
-..
 LOCAL_STATIC_LIBRARIES := libamplayer libamplayer libamcodec libavformat librtmp libavcodec libavutil libamadec_alsa libamavutils_alsa  libiconv
 LOCAL_SHARED_LIBRARIES += libutils libmedia libbinder libz libdl libcutils libssl libcrypto libasound libamsubdec
 endif
